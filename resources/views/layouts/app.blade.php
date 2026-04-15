dc5017d0d07661409dd7cbb13c981ab58ebe2bb3
alamkajoba <alamkajoba007@gmail.com>
2026-04-15 15:14:45 +0200

    fix template process

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html lang="zxx" class="js">
-
-<head>
-    <base href="../">
-    <meta charset="utf-8">
-    <meta name="author" content="Softnio">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
-    <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="./images/favicon.png">
-    <!-- Page Title  -->
-    <title>Smart Finance | Go ahead</title>
-    <!-- StyleSheets  -->
-    <link rel="stylesheet" href="{{asset('css/dashlite.css?ver=3.1.2')}}">
-    <link id="skin-default" rel="stylesheet" href="{{asset('css/theme.css?ver=3.1.2')}}">
-</head>
+@include('partials.header')
 
 <body class="nk-body ui-rounder npc-default has-sidebar ">
     <div class="nk-app-root">
